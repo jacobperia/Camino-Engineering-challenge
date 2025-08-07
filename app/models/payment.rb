1ae81fdb80bc6cd761fdaf97a370e9e1f0342c72
jacobperia <>
2025-08-06 20:42:47 -0400

    chore: added enum

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,6 @@
 class Payment < ApplicationRecord
   validates_presence_of :iticalc_id, :amount_in_cents, :last_four, :name, :status
   validates_inclusion_of :status, in: %w[Succeeded Failed Refunded]
+
+  enum status: { succeeded: 'Succeeded', failed: 'Failed', refunded: 'Refunded' }
 end
