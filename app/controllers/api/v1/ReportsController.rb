c86ec303358c36f2600a6f099715b8fdfef38b6e
jacobperia <>
2025-08-06 20:51:31 -0400

    feat: added endpoint for aggregrated report

---
+++ app/controllers/api/v1/ReportsController.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class ReportsController < ApplicationController
+      def monthly
+        report = Payment.group('date(created_at)').sum(:amount_in_cents).transform_values(&:to_i)
+
+        render json: report
+      end
+    end
+  end
+end
